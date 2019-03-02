4d887d66a913e8628f1d245afc5d76ab55ce09ad
Imipenem <philipp_ehm@protonmail.com>
2019-03-02 15:15:21 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='firstGraphProject',
+    version='1.0',
+    packages=['scripts',
+              'firstGraphProject.edge', 'firstGraphProject.node', 'firstGraphProject.graph_model'],
+    url='https://github.com/Imipenem/GraphsStructurewithPython',
+    license='',
+    author='Bitconnect',
+    author_email='',
+    description='',
+    scripts=['scripts/main.py']
+)
